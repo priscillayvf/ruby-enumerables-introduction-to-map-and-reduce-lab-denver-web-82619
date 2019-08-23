65629bb40cbc811da0a3bc8e8827364d3a338e74
Priscilla Firpo <priscillayf@gmail.com>
2019-08-23 22:38:10 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -19,9 +19,14 @@ def reduce_to_total(source_array, starting_point = 0)
 end
 
 def reduce_to_all_true(source_array)
-  source_array.reduce(true) { |truthiness, value| truthiness = !!value}
+  source_array.reduce(true, :&) { |truthiness, value|}
 end
 
 def reduce_to_any_true(source_array)
-  source_array.reduce(false) { |truthiness, value| truthiness = !!value}
+  source_array.reduce(false) do |truthiness, value|
+    if !!value == true
+      truthiness = true
+    end
+  end
 end
+
